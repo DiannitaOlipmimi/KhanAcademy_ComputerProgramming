a632f3f3bca363de11b949ed09e3bcc16a1a9198
Diannita Eka Putri <53862978+DiannitaOlipmimi@users.noreply.github.com>
2023-06-20 19:38:26 +0700

    adding documentation

---
+++ Intro to SQL_Querying and managing/SQL basics.sql
@@ -0,0 +1,37 @@
+/* Creating a table and inserting data */
+
+/** Grocery list: 
+Bananas (4)
+Peanut Butter (1)
+Dark Chocolate Bars (2)
+**/
+
+CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER );
+
+INSERT INTO groceries VALUES (1, "Bananas", 4);
+INSERT INTO groceries VALUES (2, "Peanut Butter", 1);
+INSERT INTO groceries VALUES (3, "Dark chocolate bars", 2);
+SELECT * FROM groceries;
+
+/* Challenge: Book list database */
+d INTEGER PRIMARY KEY,
+name TEXT,
+rating TEXT);
+
+INSERT INTO book VALUES(1, "Crooked House", 4);
+
+INSERT INTO book VALUES(2, "Death in the Clouds", 5);
+INSERT INTO book VALUES(3, "Death on The Nile", 4.7);
+INSERT INTO book VALUES(4, "The ABC Murders", 4.3);
+
+/* Querying the table */
+CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
+INSERT INTO groceries VALUES (1, "Bananas", 4, 7);
+INSERT INTO groceries VALUES(2, "Peanut Butter", 1, 2);
+INSERT INTO groceries VALUES(3, "Dark Chocolate Bars", 2, 2);
+INSERT INTO groceries VALUES(4, "Ice cream", 1, 12);
+INSERT INTO groceries VALUES(5, "Cherries", 6, 2);
+INSERT INTO groceries VALUES(6, "Chocolate syrup", 1, 4);
+
+SELECT * FROM groceries WHERE aisle > 5 ORDER BY aisle;
+
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # KhanAcademy_ComputerProgramming
 Computer Programming Course by Khan Academy
+
+## Documentation
+###  Creating tables
+- Many data types
+CREATE TABLE customers (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, weight REAL);
+- using primary keys
+CREATE TABLE customers (id INTEGER PRIMARY KEY, age INTEGER);
